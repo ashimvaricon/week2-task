2f7d3ce3f76b38550593d7680e77593f7036530f
ashimpoudyal <ashim1998.poudyal@gmail.com>
2024-08-14 12:07:10 +0545

    Initial Commit

---
+++ week-2-task/totalSales.js
@@ -0,0 +1,14 @@
+import salesData from "./salesData.json" assert { type: "json" };
+//We should now use salesData to calculate total price using map
+
+function totalSales(data) {
+  return data.map((item) => {
+    return {
+      product: item.product,
+      totalSale: item.quantity * item.price,
+    };
+  });
+}
+
+const total_sale = totalSales(salesData);
+console.log(total_sale);
